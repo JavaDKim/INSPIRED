b5fa726fec180a89497da6b8700d98a2c322f912
dmitry kim <javascriptkim@yandex.ru>
2023-07-08 00:14:48 +0300

    task 4 complited + ref

--- src/Components/ColorList/Color/Color.jsx
+++ src/Components/ColorList/Color/Color.jsx
@@ -1,11 +1,19 @@
+import { useEffect, useRef } from "react";
 import s from "./Color.module.scss";
 import cn from "classnames";
 
 const Color = ({ color, check }) => {
+  const refColor = useRef("");
+  useEffect(() => {
+    refColor.current.className = check ? cn(s.color, s.colorCheck) : s.color;
+    refColor.current.style = `--data-color: ${color}`;
+  }, [color]);
+
   return (
     <li
-      className={check ? cn(s.color, s.colorCheck) : s.color}
-      style={{ "--data-color": color }}
+      ref={refColor}
+      /*      className={check ? cn(s.color, s.colorCheck) : s.color}
+      style={{ "--data-color": color }}*/
     ></li>
   );
 };
